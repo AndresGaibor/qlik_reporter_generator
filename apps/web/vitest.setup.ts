87b6c3407dddcbc48365ee43c4115b78ba49502c
Andres Gaibor <androymartin99@gmail.com>
2026-08-17 20:15:53 -0500

    test(web): sincronizar notificaciones query con act

--- apps/web/vitest.setup.ts
+++ apps/web/vitest.setup.ts
@@ -1,5 +1,12 @@
+import { notifyManager } from "@tanstack/react-query";
+import { act } from "react";
+
 (
   globalThis as typeof globalThis & {
     IS_REACT_ACT_ENVIRONMENT?: boolean;
   }
 ).IS_REACT_ACT_ENVIRONMENT = true;
+
+notifyManager.setNotifyFunction((callback) => {
+  act(callback);
+});
